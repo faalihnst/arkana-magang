c2926c0a705625076ad0eec0bfbd4b3831fdd1c7
faalihnst <faalihnst@gmail.com>
2020-02-12 11:13:33 +0700

    ini

--- tes_custom/models/hr_edit.py
+++ tes_custom/models/hr_edit.py
@@ -5,9 +5,8 @@ class Employee1(models.Model):
     _description = "Costumize Employee View"
      
     no_karyawan = fields.Integer("No. Induk Karyawan", required=True)
-
-    # no_NPWP = fields.Integer("No. NPWP", required=True)
-    # no_KTP = fields.Integer("NIK", required=True)
-    # no_BPJS_Kes = fields.Integer("No. BPJS Kesehatan", required=True)
-    # no_BPJS_TK = fields.Integer("No. BPJS Ketenagakerjaan", required=True)
-    # tgl_masuk = fields.date("Tanggal Masuk", required=True)
+    no_NPWP = fields.Integer("No. NPWP", required=True)
+    no_KTP = fields.Integer("NIK", required=True)
+    no_BPJS_Kes = fields.Integer("No. BPJS Kesehatan", required=True)
+    no_BPJS_TK = fields.Integer("No. BPJS Ketenagakerjaan", required=True)
+    tgl_masuk = fields.Date("Tanggal Masuk", required=True)
